601fce822628cc2aff359b2ce2dd49554e2ab159
Diego Kuwahara de Castro <diego.castro@compasso.com.br>
2019-09-06 12:51:32 -0300

    Adição da IA da Snake no modo patrulha

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public Transform wallCheck;
+    public float speed;
+    public int health;
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2;
+    private bool isFacingRight = true;
+    private bool touchedWall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidbody2 = GetComponent<Rigidbody2D>();
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.touchedWall = Physics2D.Linecast(base.transform.position, this.wallCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (touchedWall)
+        {
+            this.Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.rigidbody2.velocity = new Vector2(speed, this.rigidbody2.velocity.y);
+    }
+
+    private void Flip()
+    {
+        this.isFacingRight = !isFacingRight;
+        base.transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        this.speed *= -1;
+    }
+}
