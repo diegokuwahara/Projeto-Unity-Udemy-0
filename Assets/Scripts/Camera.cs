0f3d5355342e4772d2bcd6418ebc2fafcd628175
Diego Kuwahara de Castro <diegookc@hotmail.com>
2019-09-08 00:33:34 -0300

    Criação de uma tela pequena para a fase 1 e movimentação da câmera

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float smoothTimeX;
+    public float smoothTimeY;
+
+    private Vector2 velocity;
+    private Transform player;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
